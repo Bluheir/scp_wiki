a7a814402b6a907507a093e584408d7037ae0200
Bluheir <gusdaybloom@gmail.com>
2025-11-22 03:38:42 +1100

    feat: create migration for profiles, roles, and tags

---
+++ supabase/migrations/20251121152811_users_and_perms.sql
@@ -0,0 +1,53 @@
+BEGIN;
+
+CREATE TABLE public.profile(
+	id UUID PRIMARY KEY,
+	FOREIGN KEY (id) REFERENCES auth.users (id) ON DELETE CASCADE,
+
+	username VARCHAR(32) NOT NULL
+);
+ALTER TABLE public.profile ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE public.urole(
+	id UUID PRIMARY KEY,
+
+	profile_id UUID,
+	FOREIGN KEY (profile_id) REFERENCES public.profile (id),
+
+	role_name VARCHAR(32) NOT NULL UNIQUE
+);
+ALTER TABLE public.urole ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE public.urole_profile(
+	profile_id UUID NOT NULL,
+	FOREIGN KEY (profile_id) REFERENCES public.profile (id) ON DELETE CASCADE,
+
+	urole_id UUID NOT NULL,
+	FOREIGN KEY (urole_id) REFERENCES public.urole (id) ON DELETE CASCADE,
+
+	PRIMARY KEY (profile_id, urole_id)
+);
+ALTER TABLE public.urole_profile ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE public.utag(
+	id UUID PRIMARY KEY,
+
+	profile_id UUID,
+	FOREIGN KEY (profile_id) REFERENCES public.profile (id),
+
+	tag_name VARCHAR(32) NOT NULL UNIQUE
+);
+ALTER TABLE public.utag ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE public.utag_urole(
+	utag_id UUID NOT NULL,
+	FOREIGN KEY (utag_id) REFERENCES public.utag (id) ON DELETE CASCADE,
+
+	urole_id UUID NOT NULL,
+	FOREIGN KEY (urole_id) REFERENCES public.urole (id) ON DELETE CASCADE,
+
+	PRIMARY KEY (utag_id, urole_id)
+);
+ALTER TABLE public.utag_urole ENABLE ROW LEVEL SECURITY;
+
+COMMIT;
