54c0b70e2735d66bf9fde7793574f226ee6a334f
Bluheir <gusdaybloom@gmail.com>
2025-11-23 15:00:49 +1100

    feat: use paraglide for text and translations

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { signInSchema } from "$lib/schema/auth"
 	import { Control, Description, Label, Field, FieldErrors } from "formsnap"
+	import { m } from "$lib/paraglide/messages"
 	import { superForm } from "sveltekit-superforms"
 	import { zod4Client } from "sveltekit-superforms/adapters"
 	import type { PageData } from "./$types"
@@ -17,34 +18,34 @@
 <div class="flex justify-center py-40 select-none">
 	<div class="px-8 py-4 hover:shadow-2xl transition rounded-box bg-base-200 flex border border-base-content/10 w-230 gap-6">
 		<form method="POST" use:enhance class="card w-sm gap-y-1.5 py-4">
-			<h1 class="bold text-2xl font-bold">Welcome back Doctor [REDACTED]</h1>
+			<h1 class="bold text-2xl font-bold">{m.login_welcomeBack()}</h1>
 			<Field {form} name="email">
 				<Control>
 					{#snippet children({ props })}
-						<Label class="label text-xs">Email</Label>
+						<Label class="label text-xs">{m.auth_email()}</Label>
 						<label class="input input-sm w-full">
 							<Mail class="h-[1em] text-sm" />
-							<input type="email" {...props} bind:value={$formData.email} placeholder="Email" />
+							<input type="email" {...props} bind:value={$formData.email} placeholder="{m.auth_email()}" />
 						</label>
 					{/snippet}
 				</Control>
-				<Description class="text-xs">Email address for the account</Description>
+				<Description class="text-xs">{m.auth_emailDescription()}</Description>
 				<FieldErrors class="text-xs text-error"/>
 			</Field>
 			<Field {form} name="password">
 				<Control>
 					{#snippet children({ props })}
-						<Label class="label text-xs">Password</Label>
+						<Label class="label text-xs">{m.auth_password()}</Label>
 						<label class="input input-sm w-full">
 							<Lock class="h-[1em] text-sm"/>
-							<input type="password" {...props} bind:value={$formData.password} placeholder="Password" />
+							<input type="password" {...props} bind:value={$formData.password} placeholder={m.auth_password()} />
 						</label>
 					{/snippet}
 				</Control>
-				<Description class="text-xs">Account password</Description>
+				<Description class="text-xs">{m.auth_passwordDescription()}</Description>
 				<FieldErrors class="text-xs text-error"/>
 			</Field>
-			<input class="btn btn-primary btn-sm" type="submit" value="Sign in" />
+			<input class="btn btn-primary btn-sm" type="submit" value={m.login_signIn()} />
 		</form>
 		<div class="h-full w-[0.5px] bg-base-content/10"></div>
 		<div class="flex items-center gap-3">
