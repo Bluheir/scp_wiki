fcda46b1bd85afdf0615011ae9a423c6935b9d3f
Bluheir <gusdaybloom@gmail.com>
2025-11-29 00:26:47 +1100

    feat: create register form page content

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,6 +3,10 @@
 	import type { PageData } from "./$types"
 	import { registerSchema } from "$lib/schema/auth"
 	import { superForm } from "sveltekit-superforms"
+	import { Control, Description, Field, FieldErrors, Label } from "formsnap"
+	import { Mail, Lock, User } from "lucide-svelte"
+	import SCPEmblem from "$lib/icons/SCPEmblem.svelte"
+	import { m } from "$lib/paraglide/messages"
 
 	let { data }: { data: PageData } = $props()
 
@@ -11,3 +15,61 @@
 	})
 	const { form: formData, enhance } = form
 </script>
+
+<div class="flex justify-center py-40 select-none">
+	<div class="px-6 py-4 shadow-2xl transition rounded-box bg-base-200 flex border border-base-content/10 gap-6 font-mono">
+		<form method="POST" use:enhance class="card w-sm gap-y-1.5 py-4">
+			<h1 class="bold text-2xl font-bold">{m.register_title()}</h1>
+			<Field {form} name="email">
+				<Control>
+					{#snippet children({ props })}
+						<Label class="label text-xs">{m.auth_email()}</Label>
+						<label class="input input-sm w-full">
+							<Mail class="h-[1em] text-sm" />
+							<input type="email" {...props} bind:value={$formData.email} placeholder="{m.auth_email()}" />
+						</label>
+					{/snippet}
+				</Control>
+				<Description class="text-xs">{m.auth_emailDescription()}</Description>
+				<FieldErrors class="text-xs text-error"/>
+			</Field>
+			<Field {form} name="username">
+				<Control>
+					{#snippet children({ props })}
+						<Label class="label text-xs">{m.register_username()}</Label>
+						<label class="input input-sm w-full">
+							<User class="h-[1em] text-sm" />
+							<input type="text" {...props} bind:value={$formData.username} placeholder="{m.register_username()}" />
+						</label>
+					{/snippet}
+				</Control>
+				<Description class="text-xs">{m.register_usernameDescription()}</Description>
+				<FieldErrors class="text-xs text-error"/>
+			</Field>
+			<Field {form} name="password">
+				<Control>
+					{#snippet children({ props })}
+						<Label class="label text-xs">{m.auth_password()}</Label>
+						<label class="input input-sm w-full">
+							<Lock class="h-[1em] text-sm"/>
+							<input type="password" {...props} bind:value={$formData.password} placeholder={m.auth_password()} />
+						</label>
+					{/snippet}
+				</Control>
+				<Description class="text-xs">{m.auth_passwordDescription()}</Description>
+				<FieldErrors class="text-xs text-error"/>
+			</Field>
+			<input class="btn btn-primary btn-sm" type="submit" value={m.register_register()} />
+		</form>
+		<div class="h-full w-[0.5px] bg-base-content/10"></div>
+		<div class="flex items-center rounded-box">
+			<div class="flex items-center gap-4">
+				<SCPEmblem class="text-base-content h-40 motion-safe:animate-spin-slow"/>
+				<div class="grow">
+					<h2 class="text-4xl font-bauhaus">SCP Foundation</h2>
+					<h3 class="text-xl">Secure Contain Protect</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
