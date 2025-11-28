12ba7e432084eeec8845a79119a290ff2b6a264c
Bluheir <gusdaybloom@gmail.com>
2025-11-29 00:06:21 +1100

    feat: form initialization in register page svelte file

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { zod4Client } from "sveltekit-superforms/adapters"
+	import type { PageData } from "./$types"
+	import { registerSchema } from "$lib/schema/auth"
+	import { superForm } from "sveltekit-superforms"
+
+	let { data }: { data: PageData } = $props()
+
+	const form = superForm(data.form, {
+		validators: zod4Client(registerSchema)
+	})
+	const { form: formData, enhance } = form
+</script>
