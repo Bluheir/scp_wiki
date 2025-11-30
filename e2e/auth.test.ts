0074d9df5e920fbffb61237f84a4f588caf7df07
Bluheir <gusdaybloom@gmail.com>
2025-11-30 15:41:03 +1100

    test: implement tests for bad form data for registering a user, signing in successfully, and signing in with an invalid email and password combination

--- e2e/auth.test.ts
+++ e2e/auth.test.ts
@@ -1,9 +1,7 @@
-import { expect, test } from "@playwright/test"
-import { type SupabaseClient, createClient } from "@supabase/supabase-js"
+import { expect, test } from "./fixtures"
 
 test.describe("register", () => {
 	test("valid data successfully registers user", async ({ page }) => {
-		console.log(process.env)
 		const email = `test${Date.now()}@example.com`
 		const password = "12345aA!"
 
@@ -18,5 +16,52 @@ test.describe("register", () => {
 		await page.locator("[type=submit]").click()
 
 		await expect(page.getByTestId("register-success-message")).toBeVisible()
+	})
+
+	test("existing email does not register user", async ({ page, adminClient }) => {
+		const email = `test${Date.now()}@example.com`
+		const password = "12345aA!"
+		await adminClient.auth.signUp({ email, password })
+
+		await page.goto("/register")
+		await page.getByLabel("Email").fill(email)
+		await page.getByLabel("Username").fill("testUser")
+		await page.getByLabel("Password", { exact: true }).fill(password)
+		await page.getByLabel("Confirm password").fill(password)
+
+		await expect(page.getByTestId("register-success-message")).toBeHidden()
+
+		await page.locator("[type=submit]").click()
+
+		await expect(page.getByTestId("register-success-message")).toBeHidden()
+	})
+})
+
+test.describe("login", () => {
+	test("correct email and password logs in", async ({ page, adminClient }) => {
+		const email = `test${Date.now()}@example.com`
+		const password = "12345aA!"
+		await adminClient.auth.signUp({ email, password, options: { data: { username: "test" } } })
+
+		await page.goto("/login")
+		await page.getByLabel("Email").fill(email)
+		await page.getByLabel("Password").fill(password)
+
+		await page.locator("[type=submit]").click()
+
+		await expect(page).toHaveURL("/")
+	})
+
+	test("invalid email and password does not log in", async ({ page }) => {
+		const email = `test${Date.now()}@example.com`
+		const password = "12345aA!"
+
+		await page.goto("/login")
+		await page.getByLabel("Email").fill(email)
+		await page.getByLabel("Password").fill(password)
+
+		await page.locator("[type=submit]").click()
+
+		await expect(page).toHaveURL("/login")
 	})
 })
