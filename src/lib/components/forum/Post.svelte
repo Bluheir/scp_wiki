0cf6965a8ecd0ca38ebdd434fe241370e778556e
Bluheir <gusdaybloom@gmail.com>
2025-12-01 03:04:59 +1100

    style: format Post.svelte with Prettier

--- src/lib/components/forum/Post.svelte
+++ src/lib/components/forum/Post.svelte
@@ -4,11 +4,14 @@
 	import { ChevronDown, ChevronUp, Pencil, Reply, X } from "lucide-svelte"
 	import { m } from "$lib/paraglide/messages"
 
-	const { post, actionItems }: {
-		post: Post,
+	const {
+		post,
+		actionItems
+	}: {
+		post: Post
 		actionItems: {
-			replyAble: boolean,
-			editAble: boolean,
+			replyAble: boolean
+			editAble: boolean
 			deleteAble: boolean
 		}
 	} = $props()
@@ -25,59 +28,73 @@
 	}
 
 	function segmentNumber(n: number): string[] {
-		const s = String(n);
-		const out: string[] = [];
+		const s = String(n)
+		const out: string[] = []
 
-		let i = s.length;
+		let i = s.length
 		while (i > 3) {
-			const end = i;
-			const start = i - 3;
-			out.unshift(s.slice(start, end));
-			out.unshift(",");
-			i -= 3;
+			const end = i
+			const start = i - 3
+			out.unshift(s.slice(start, end))
+			out.unshift(",")
+			i -= 3
 		}
 
-		out.unshift(s.slice(0, i));
-		return out;
+		out.unshift(s.slice(0, i))
+		return out
 	}
 </script>
 
 <div class="rounded-box border border-base-content/10 p-4 transition hover:shadow-lg">
 	<div class="flex items-center gap-4">
-		<div class="flex items-center flex-col">
-			<button class="btn btn-square btn-xs {myRating === 1 ? "text-primary" : "btn-ghost"}" onclick={async () => {
-				if(myRating === 1) {
-					await setNewRating(0)
-				} else {
-					await setNewRating(1)
-				}
-			}} title={m.forum_post_upvote()}><ChevronUp /></button>
+		<div class="flex flex-col items-center">
+			<button
+				class="btn btn-square btn-xs {myRating === 1 ? 'text-primary' : 'btn-ghost'}"
+				onclick={async () => {
+					if (myRating === 1) {
+						await setNewRating(0)
+					} else {
+						await setNewRating(1)
+					}
+				}}
+				title={m.forum_post_upvote()}><ChevronUp /></button
+			>
 			<div>
 				{#each segmentNumber(rating) as segment, index}
 					{#if segment === ","}
 						{segment}
 					{:else}
-						<span class="countdown"><span style="--value:{segment};{index === 0 ? "" : "--digits:3;"}">{segment}</span></span>
+						<span class="countdown"
+							><span style="--value:{segment};{index === 0 ? '' : '--digits:3;'}">{segment}</span
+							></span
+						>
 					{/if}
 				{/each}
 			</div>
-			<button class="btn btn-square btn-xs {myRating === -1 ? "text-primary" : "btn-ghost"}" onclick={async () => {
-				if(myRating === -1) {
-					await setNewRating(0)
-				} else {
-					await setNewRating(-1)
-				}
-			}} title={m.forum_post_downvote()}><ChevronDown /></button>
+			<button
+				class="btn btn-square btn-xs {myRating === -1 ? 'text-primary' : 'btn-ghost'}"
+				onclick={async () => {
+					if (myRating === -1) {
+						await setNewRating(0)
+					} else {
+						await setNewRating(-1)
+					}
+				}}
+				title={m.forum_post_downvote()}><ChevronDown /></button
+			>
 		</div>
 		<Avatar.Root class="avatar">
 			<div class="w-12 rounded-box">
 				<Avatar.Image src={post.authorProfileSrc} />
-				<Avatar.Fallback class="select-none h-full flex justify-center items-center border border-base-content/10 rounded-box bg-base-200">{post.authorUsername[0]}</Avatar.Fallback>
+				<Avatar.Fallback
+					class="flex h-full items-center justify-center rounded-box border border-base-content/10 bg-base-200 select-none"
+					>{post.authorUsername[0]}</Avatar.Fallback
+				>
 			</div>
 		</Avatar.Root>
 		<div class="text-lg font-bold">{post.authorUsername}</div>
 	</div>
-	<div class="text-xs text-base-content/90 my-2">{post.created.toLocaleString()}</div>
+	<div class="my-2 text-xs text-base-content/90">{post.created.toLocaleString()}</div>
 	<Separator.Root
 		class="my-4 h-px bg-base-content/10 [orientation='vertical']:h-full"
 		orientation="horizontal"
@@ -88,13 +105,19 @@
 		</div>
 		<div class="flex gap-2">
 			{#if actionItems.replyAble}
-				<button class="btn btn-xs gap-1 transition" title={m.forum_post_reply()}><Reply class="w-[1.5em]" />{m.forum_post_reply()}</button>
+				<button class="btn gap-1 transition btn-xs" title={m.forum_post_reply()}
+					><Reply class="w-[1.5em]" />{m.forum_post_reply()}</button
+				>
 			{/if}
 			{#if actionItems.editAble}
-				<button class="btn btn-xs gap-1 transition" title={m.forum_post_edit()}><Pencil class="w-[1.4em]" />{m.forum_post_edit()}</button>
+				<button class="btn gap-1 transition btn-xs" title={m.forum_post_edit()}
+					><Pencil class="w-[1.4em]" />{m.forum_post_edit()}</button
+				>
 			{/if}
 			{#if actionItems.deleteAble}
-				<button class="btn btn-xs btn-error gap-1 transition" title={m.forum_post_delete()}><X class="w-[1.5em]" />{m.forum_post_delete()}</button>
+				<button class="btn gap-1 transition btn-xs btn-error" title={m.forum_post_delete()}
+					><X class="w-[1.5em]" />{m.forum_post_delete()}</button
+				>
 			{/if}
 		</div>
 	</div>
@@ -103,8 +126,12 @@
 <style>
 	.countdown {
 		& > * {
-			&:after, &:before {
-				transition: all 0.3s cubic-bezier(1, 0, 0, 1), width 0.2s ease-out 0.2s, opacity 0.2s ease-out 0.2s;
+			&:after,
+			&:before {
+				transition:
+					all 0.3s cubic-bezier(1, 0, 0, 1),
+					width 0.2s ease-out 0.2s,
+					opacity 0.2s ease-out 0.2s;
 			}
 		}
 	}
