4432ed457d51f7b27fb161cf93fca3d01c9b577b
MikuFanGirl <slimcognitodudesvids@gmail.com>
2025-12-07 17:02:53 +1100

    feat(storybook): create a Storybook story for the Navbar component

---
+++ src/stories/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Navbar from "$lib/components/Navbar.svelte"
+	import type { string } from "zod"
+
+	const props: {
+		id: string
+		avatarUrl?: string,
+		username: string
+	} | {
+		id: undefined
+	} = $props()
+</script>
+
+<div class="w-[1000px]">
+	{#if props.id}
+		<Navbar user={props} />
+	{:else}
+		<Navbar />
+	{/if}
+</div>
