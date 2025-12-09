fbef58d85ce07cfbdb8e04d274ad851a9dc8e652
MikuFanGirl <slimcognitodudesvids@gmail.com>
2025-12-09 19:03:24 +1100

    feat(storybook): create the Storybook story for the Profile component

---
+++ src/stories/Profile.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { Profile } from "$lib/components/profile/profile"
+	import ProfileC from "$lib/components/profile/Profile.svelte"
+
+	const profile: Profile = $props()
+</script>
+
+<ProfileC {profile} />
