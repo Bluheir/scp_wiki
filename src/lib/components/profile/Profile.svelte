3bfbec76c6622b3886ed0715f10d82a9114d87dc
MikuFanGirl <slimcognitodudesvids@gmail.com>
2025-12-12 00:17:34 +1100

    feat: separate editing and viewing profile functionality into different files and use formsnap for errors

--- src/lib/components/profile/Profile.svelte
+++ src/lib/components/profile/Profile.svelte
@@ -3,91 +3,32 @@
 	import type { Profile } from "./profile"
 	import { m } from "$lib/paraglide/messages"
 	import UserAvatar from "../UserAvatar.svelte"
-	import { Pencil, Save } from "lucide-svelte"
+	import { superValidate, type SuperValidated } from "sveltekit-superforms"
+	import ProfileEdit from "./ProfileEdit.svelte"
+	import { Pencil } from "lucide-svelte"
+	import { zod4 } from "sveltekit-superforms/adapters"
+	import { profileSchema } from "$lib/schema/profile"
 
 	let { profile, readonly = false }: { profile: Profile; readonly?: boolean } = $props()
-	let { username, biography, pronouns } = $derived(profile)
-
-	let pronounsVisible = $derived(pronouns.trim().length !== 0)
 	let editMode:
-		| {
+		| SuperValidated<
+			{
 				username: string
 				pronouns: string
 				biography: string
-		  }
+			},
+			any,
+			{
+				username: string
+				pronouns: string
+				biography: string
+			}
+		>
 		| undefined = $state()
 	const totalRating = $derived(profile.wikiRating + profile.forumRating)
 </script>
 
-<div
-	class="prose max-w-[unset] prose-h2:my-0 prose-h3:mt-4 prose-table:my-4 prose-table:text-base prose-table:text-base-content prose-img:my-0"
->
-	<div class="flex items-center gap-4">
-		<UserAvatar
-			user={{
-				id: profile.id,
-				username,
-				avatarUrl: profile.avatarUrl
-			}}
-			size="lg"
-			style="box"
-		/>
-		<Tooltip.Provider>
-			{#if editMode}
-				<input
-					class="input block min-w-[unset] text-2xl font-bold text-base-content"
-					placeholder="Username"
-					bind:value={editMode.username}
-				/>
-			{:else}
-				<Tooltip.Root delayDuration={200}>
-					<Tooltip.Trigger class="select-all">
-						<h2>{username}</h2>
-					</Tooltip.Trigger>
-					<Tooltip.Content sideOffset={8}>
-						<div
-							class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
-						>
-							{m.profile_username()}
-						</div>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			{/if}
-			{#if pronounsVisible && !editMode}
-				<Tooltip.Root delayDuration={200}>
-					<Tooltip.Trigger class="select-all">
-						{pronouns}
-					</Tooltip.Trigger>
-					<Tooltip.Content sideOffset={8}>
-						<div
-							class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
-						>
-							{m.profile_pronouns()}
-						</div>
-					</Tooltip.Content>
-				</Tooltip.Root>
-			{:else if editMode}
-				<input
-					class="input w-40"
-					type="text"
-					placeholder={m.profile_pronouns()}
-					bind:value={editMode.pronouns}
-				/>
-			{/if}
-			<Tooltip.Root delayDuration={200}>
-				<Tooltip.Trigger class="select-all">
-					<div class="rounded-box bg-base-200 px-2">{profile.createdAt.toLocaleDateString()}</div>
-				</Tooltip.Trigger>
-				<Tooltip.Content sideOffset={8}>
-					<div
-						class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
-					>
-						{m.profile_joined()}
-					</div>
-				</Tooltip.Content>
-			</Tooltip.Root>
-		</Tooltip.Provider>
-	</div>
+{#snippet ratingTableS()}
 	<table>
 		<thead>
 			<tr>
@@ -104,52 +45,93 @@
 			</tr>
 		</tbody>
 	</table>
-	<section>
-		<h3>{m.profile_biography()}</h3>
-		{#if editMode}
-			<textarea
-				class="textarea min-h-80 w-full text-base"
-				contenteditable="plaintext-only"
-				bind:value={editMode.biography}
-				placeholder={m.profile_biography()}
-			>
-			</textarea>
-		{:else}
-			<p>{biography}</p>
-		{/if}
-	</section>
-	<div class="my-6 flex gap-2">
-		{#if editMode}
-			<button
-				class="btn btn-sm btn-primary"
-				onclick={() => {
-					username = editMode!.username
-					biography = editMode!.biography
-					pronouns = editMode!.pronouns
-					console.log(biography)
-					editMode = undefined
-				}}><Save class="w-[1em]" />Save</button
-			>
-		{/if}
-		{#if !readonly && !editMode}
+{/snippet}
+
+<div
+	class="prose max-w-[unset] prose-h2:my-0 prose-h3:mt-4 prose-table:my-4 prose-table:text-base prose-table:text-base-content prose-img:my-0"
+>
+	{#if !editMode}
+		<div class="flex items-center gap-4">
+			<UserAvatar user={profile} size="lg" style="box" />
+			<Tooltip.Provider>
+				<Tooltip.Root delayDuration={200}>
+					<Tooltip.Trigger class="select-all">
+						<h2>{profile.username}</h2>
+					</Tooltip.Trigger>
+					<Tooltip.Content sideOffset={8}>
+						<div
+							class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
+						>
+							{m.profile_username()}
+						</div>
+					</Tooltip.Content>
+				</Tooltip.Root>
+				<Tooltip.Root delayDuration={200}>
+					<Tooltip.Trigger class="select-all">
+						{profile.pronouns}
+					</Tooltip.Trigger>
+					<Tooltip.Content sideOffset={8}>
+						<div
+							class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
+						>
+							{m.profile_pronouns()}
+						</div>
+					</Tooltip.Content>
+				</Tooltip.Root>
+				<Tooltip.Root delayDuration={200}>
+					<Tooltip.Trigger class="select-all">
+						<div class="rounded-box bg-base-200 px-2">{profile.createdAt.toLocaleDateString()}</div>
+					</Tooltip.Trigger>
+					<Tooltip.Content sideOffset={8}>
+						<div
+							class="shadow-popover z-0 flex items-center justify-center rounded-box border border-base-content/10 bg-base-200 p-2 text-sm font-medium outline-hidden"
+						>
+							{m.profile_joined()}
+						</div>
+					</Tooltip.Content>
+				</Tooltip.Root>
+			</Tooltip.Provider>
+		</div>
+		{@render ratingTableS()}
+		<div>
+			<h3>{m.profile_biography()}</h3>
+			<p>
+				{profile.biography}
+			</p>
+		</div>
+		{#if !readonly}
+		<div class="my-6 flex gap-2">
 			<button
 				class="btn btn-outline btn-sm"
-				onclick={() => {
-					editMode = {
-						username,
-						pronouns,
-						biography
-					}
-				}}><Pencil class="w-[1em]" />Edit profile</button
+				onclick={async () => {
+					const formValidated = await superValidate(
+						zod4(profileSchema),
+						{
+							defaults: {
+								username: profile.username,
+								biography: profile.biography,
+								pronouns: profile.pronouns
+							}
+						}
+					)
+					editMode = formValidated
+				}}
 			>
+				<Pencil class="w-[1em]" />
+				Edit profile
+			</button>
+		</div>
 		{/if}
-		{#if !readonly && editMode}
-			<button
-				class="btn btn-sm btn-error"
-				onclick={() => {
-					editMode = undefined
-				}}><Pencil class="w-[1em]" />Discard changes</button
-			>
-		{/if}
-	</div>
+	{:else}
+		<ProfileEdit
+			onDiscard={() => (editMode = undefined)}
+			onSubmit={(data) => console.log(data)}
+			formValidated={editMode}
+			{profile}
+		>
+			{#snippet ratingTable()}
+				{@render ratingTableS()}
+			{/snippet}
+		</ProfileEdit>
+	{/if}
 </div>
---
+++ src/lib/components/profile/ProfileEdit.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+	import UserAvatar from "$lib/components/UserAvatar.svelte"
+	import { superForm, type SuperValidated } from "sveltekit-superforms"
+	import type { Profile, ProfileEdit } from "./profile"
+	import * as Form from "formsnap"
+	import { m } from "$lib/paraglide/messages"
+	import type { Snippet } from "svelte"
+	import { Pencil, Save } from "lucide-svelte"
+	import { zodClient } from "sveltekit-superforms/adapters"
+	import { profileSchema } from "$lib/schema/profile"
+
+	let {
+		profile,
+		formValidated,
+		onDiscard,
+		onSubmit,
+		ratingTable
+	}: {
+		profile: Profile
+		onDiscard: () => Promise<void> | void
+		onSubmit: (data: ProfileEdit) => Promise<void> | void
+		ratingTable: Snippet<[]>
+		formValidated: SuperValidated<ProfileEdit, any, ProfileEdit>
+	} = $props()
+
+	const form = superForm(formValidated, {
+		validators: zodClient(profileSchema)
+	})
+
+	const { form: formData, enhance } = $derived(form)
+</script>
+
+<form
+	onsubmit={async (e) => {
+		e.preventDefault()
+		await onSubmit($formData)
+	}}
+	use:enhance
+>
+	<div class="flex gap-4">
+		<div>
+			<UserAvatar
+				user={{
+					id: profile.id,
+					username: $formData.username,
+					avatarUrl: profile.avatarUrl
+				}}
+				size="lg"
+				style="box"
+			/>
+		</div>
+		<div class="flex gap-4">
+			<div class="w-80">
+				<Form.Field {form} name="username">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label class="font-bold text-base-content">{m.register_username()}</Form.Label>
+							<input
+								class="input"
+								placeholder={m.register_username()}
+								{...props}
+								bind:value={$formData.username}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors class="text-error" />
+				</Form.Field>
+			</div>
+			<div class="w-80">
+				<Form.Field {form} name="pronouns">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label class="font-bold text-base-content">{m.profile_pronouns()}</Form.Label>
+							<input
+								class="input"
+								type="text"
+								placeholder={m.profile_pronouns()}
+								bind:value={$formData.pronouns}
+								{...props}
+							/>
+						{/snippet}
+					</Form.Control>
+					<Form.FieldErrors class="text-error" />
+				</Form.Field>
+			</div>
+		</div>
+	</div>
+	{@render ratingTable()}
+	<section>
+		<Form.Field {form} name="biography">
+			<Form.Control>
+				{#snippet children({ props })}
+					<h3><Form.Label>{m.profile_biography()}</Form.Label></h3>
+					<textarea
+						class="textarea min-h-80 w-full text-base"
+						contenteditable="plaintext-only"
+						bind:value={$formData.biography}
+						placeholder={m.profile_biography()}
+						{...props}
+					>
+					</textarea>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors class="my-3 text-error" />
+		</Form.Field>
+	</section>
+	<div class="flex gap-2">
+		<button type="submit" class="btn btn-sm btn-primary"><Save class="w-[1em]" />Save</button>
+		<button class="btn btn-sm btn-error" onclick={onDiscard}>
+			<Pencil class="w-[1em]" />
+			Discard changes
+		</button>
+	</div>
+</form>
