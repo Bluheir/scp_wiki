8d7bc08d924fbc8c9a6695d0cf37e945588da522
CJ-programming <coreycooldude99@gmail.com>
2025-12-11 19:54:05 +1100

    chore: change all fields to be readonly

--- src/lib/components/forum/types.ts
+++ src/lib/components/forum/types.ts
@@ -2,27 +2,28 @@ export type Topic = ImmediateParentTopic | ParentTopic
 
 export type ImmediateParentTopic = {
 	readonly id: string
-	title: string
-	description: string
+	readonly title: string
+	readonly description: string
 	readonly type: "immediateParent"
 	children: PostPreview[]
 }
 
 export type ParentTopic = {
 	readonly id: string
-	title: string
-	description: string
+	readonly title: string
+	readonly description: string
 	readonly type: "parent"
 	children: ChildTopic[]
 }
 
 export type ChildTopic = {
 	readonly id: string
+	readonly title: string
+	readonly description: string
 	readonly threadAmount: number
 	readonly postAmount: number
 	readonly lastPoster: Author
-	title: string
-	description: string
+	readonly timestamp: number
 }
 
 export type PostPreview = {
@@ -30,9 +31,9 @@ export type PostPreview = {
 	readonly author: Author
 	readonly lastReply: Author
 	readonly repliesAmount: number
-	title: string
-	preview: string
-	timestamp: number
+	readonly title: string
+	readonly preview: string
+	readonly timestamp: number
 }
 
 export type Author = {
