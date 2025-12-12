9dcd1f32f6a0ceb05f8a3353666c0f49106f4ca0
MikuFanGirl <slimcognitodudesvids@gmail.com>
2025-12-12 17:08:48 +1100

    fix: create `uuidv7` function and use it in the `handle_new_user()` trigger
    because the `pg_uuidv7` extension is unavailable

--- supabase/migrations/20251212035425_default_roles.sql
+++ supabase/migrations/20251212035425_default_roles.sql
@@ -1,6 +1,43 @@
 begin;
 
-create extension if not exists pg_uuidv7;
+create or replace function public.uuidv7()
+returns uuid
+language plpgsql
+security definer set search_path = ''
+as $$
+declare
+	unix_ts_ms bigint;
+	rand_bytes bytea;
+	b bytea;
+begin
+	unix_ts_ms := floor(
+		extract(
+			epoch
+			from clock_timestamp()
+		) * 1000
+	);
+
+	rand_bytes := gen_random_bytes(12);
+	b := repeat(E'\\000', 16)::bytea;
+
+	for i in 0..5 loop
+		b := set_byte(b, i, (unix_ts_ms >> ((5 - i) * 8)) & 255);
+	end loop;
+
+	b := set_byte(
+		b,
+		6,
+		(7 << 4) | ((get_byte(rand_bytes, 0) & 0x0F))
+	);
+
+	b := set_byte(b, 7, (0x80 | (get_byte(rand_bytes, 1) & 0x3F)));
+
+	for i in 2..9 loop
+		b := set_byte(b, 6 + i, get_byte(rand_bytes, i));
+	end loop;
+	return encode(b, 'hex')::uuid;
+end;
+$$;
 
 alter table public.urole_custom
 add column is_default bool not null default false;
@@ -36,7 +73,7 @@ begin
 			urole_custom.is_default;
 
 	with new_urole as (
-		insert into public.urole (id) select uuid_generate_v7() returning *
+		insert into public.urole (id) select public.uuidv7() returning *
 	),
 	new_urole_user as (
 		insert into public.user_urole (id, profile_id)
@@ -66,7 +103,7 @@ begin
 	)
 	insert into public.permission_action (id, role_id, action_type, action_data)
 	select
-		uuid_generate_v7(),
+		public.uuidv7(),
 		new_urole.id,
 		default_permission_action.action_type,
 		default_permission_action.action_data
