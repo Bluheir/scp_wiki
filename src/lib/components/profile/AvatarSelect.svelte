4ccc5ecc7f6b9b5cd61528f97e8486a5a20c9045
MikuFanGirl <slimcognitodudesvids@gmail.com>
2025-12-15 15:03:22 +1100

    fix: use Paraglide keys for the `AvatarSelect` component

--- src/lib/components/profile/AvatarSelect.svelte
+++ src/lib/components/profile/AvatarSelect.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { m } from "$lib/paraglide/messages"
 	import Cropper from "svelte-easy-crop"
 
 	type ImageData = {
@@ -37,20 +38,20 @@
 			await onsubmit(image!)
 		}
 	}}>
-	<h3 class="text-2xl font-bold text-base-content">Avatar Image</h3>
+	<h3 class="text-2xl font-bold text-base-content">{m.profile_avatar_title()}</h3>
 	{#if !image}
 		<fieldset class="fieldset">
-			<legend class="fieldset-legend">Upload a profile picture</legend>
+			<legend class="fieldset-legend">{m.profile_avatar_description()}</legend>
 			<input
 				type="file"
 				class="file-input"
 				accept="image/webp,image/jpeg,image/png"
 				onchange={handleFile}
 			/>
-			<span class="label">Max size 0.5MB</span>
+			<span class="label">{m.profile_avatar_sizeHint()}</span>
 		</fieldset>
 	{:else}
-		<p class="text-base-content/50 text-sm my-2">Select the area of the image you want to display as your avatar</p>
+		<p class="text-base-content/50 text-sm my-2">{m.profile_avatar_selectionHint()}</p>
 		<div class="flex justify-center my-6">
 			<div class="h-[256px] w-[256px] relative">
 				<Cropper
@@ -67,11 +68,11 @@
 			</div>
 		</div>
 		<div class="flex gap-2">
-			<input class="btn btn-sm btn-primary flex-1" type="submit" value="Submit"/>
+			<input class="btn btn-sm btn-primary flex-1" type="submit" value={m.profile_avatar_submit()}/>
 			<button class="btn btn-sm btn-error flex-1" onclick={() => {
 				URL.revokeObjectURL(image!.image)
 				image = undefined
-			}}>Choose another image</button>
+			}}>{m.profile_avatar_back()}</button>
 		</div>
 	{/if}
 </form>
