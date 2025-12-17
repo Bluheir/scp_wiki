2cfd4b18fd1f98ef0d4b3a17fe9bcd310f2e543b
MikuFanGirl <44828102+Bluheir@users.noreply.github.com>
2025-12-18 00:46:50 +1100

    feat: handle file upload on the backend

--- src/routes/profile/[userId]/+page.server.ts
+++ src/routes/profile/[userId]/+page.server.ts
@@ -1,10 +1,86 @@
 import { profileSchema } from "$lib/components/profile/profile"
-import type { Actions } from "@sveltejs/kit"
+import { error, type Actions } from "@sveltejs/kit"
+import sharp, { type Sharp } from "sharp"
+import { Readable } from "node:stream"
+import { ReadableStream } from "node:stream/web"
 import { fail, message, superValidate } from "sveltekit-superforms"
 import { zod4 } from "sveltekit-superforms/adapters"
+import { v7 } from "uuid"
+
+function relativePositions(
+	meta: {
+		width: number
+		height: number
+		naturalWidth: number
+		naturalHeight: number
+	},
+	zoom: number,
+	crop: { x: number; y: number },
+	cropSize: { width: number, height: number }
+) {
+	const width = Math.round(meta.naturalWidth / zoom)
+	const height = Math.round(meta.naturalHeight / zoom)
+	const x = Math.round(
+		(((meta.width - cropSize.width / zoom) / 2 - crop.x / zoom) / meta.width) * meta.naturalWidth
+	)
+	const y = Math.round(
+		(((meta.height - cropSize.height / zoom) / 2 - crop.y / zoom) / meta.height) * meta.naturalHeight
+	)
+
+	let m = { width, height }
+	if(meta.naturalWidth >= meta.naturalHeight) {
+		m.width = height
+	} else {
+		m.height = width
+	}
+
+	return {
+		...m,
+		x,
+		y
+	}
+}
+
+function imageEdit(
+	meta: sharp.Metadata,
+	crop: { x: number; y: number },
+	zoom: number,
+	size: number
+): Sharp | undefined {
+	const { x, y, width, height } = relativePositions({
+		width: size,
+		height: size,
+		naturalWidth: meta.width,
+		naturalHeight: meta.height
+	}, zoom, crop, { width: size, height: size })
+
+	if (x + width > meta.width || y + height > meta.height || x < 0 || y < 0) {
+		return
+	}
+
+	return sharp()
+		.extract({
+			top: y,
+			left: x,
+			height,
+			width
+		})
+		.resize({
+			width: size,
+			height: size
+		})
+		.avif({
+			lossless: true,
+			quality: 100
+		})
+}
 
 export const actions: Actions = {
 	default: async ({ request, locals, params }) => {
+		if(!params.userId) {
+			error(400)
+		}
+		const userId = params.userId
 		const profileSchemaServer = profileSchema.check(async ({ value, issues }) => {
 			const result = await locals.supabaseUser
 				.from("profile")
@@ -13,7 +89,50 @@ export const actions: Actions = {
 					username: value.username,
 					pronouns: value.pronouns
 				})
-				.eq("id", params.userId!)
+				.eq("id", userId)
+				.select("id")
+				.single()
+
+			if (!result.data) {
+				issues.push({
+					code: "custom",
+					message: "Failed to update profile, most likely because permissions were changed",
+					path: ["username"],
+					input: userId
+				})
+				return
+			}
+
+			if (!value.imageData) {
+				return
+			}
+
+			const listed = await locals.supabaseAdmin.storage.from("avatar").list(`${userId}`)
+			if(!listed.data) {
+				return
+			}
+
+			if(listed.data.length >= 3) {
+				const idsToDelete = listed.data.filter(value => value.name !== ".emptyFolderPlaceholder").map(value => value.name).sort((a, b) => b.localeCompare(a)).slice(1)
+				await locals.supabaseAdmin.storage.from("avatar").remove(idsToDelete.map(name => `${userId}/${name}`))
+			}
+
+			const { imageData } = value
+			const imageStream = imageData.image.stream()
+			const [imageMetadataStream, mainStream] = imageStream.tee()
+			const metadata = await Readable.fromWeb(imageMetadataStream as ReadableStream).pipe(sharp()).metadata();
+			const pipeline = imageEdit(metadata, imageData.crop, imageData.zoom, 256)
+			if(!pipeline) {
+				return
+			}
+			const avifStream = Readable.fromWeb(mainStream as ReadableStream).pipe(pipeline)
+			const generatedUuid = v7()
+
+			const newAvatarPath = `${params.userId}/${generatedUuid}.avif`
+			await locals.supabaseAdmin.storage.from("avatar").upload(newAvatarPath, avifStream, { contentType: "image/avif" })
+			const newAvatarUrl = locals.supabaseAdmin.storage.from("avatar").getPublicUrl(newAvatarPath).data.publicUrl
+			
+			await locals.supabaseAdmin.from("profile").update({ avatar_url: newAvatarUrl }).eq("id", userId)
 		})
 
 		const form = await superValidate(request, zod4(profileSchemaServer))
