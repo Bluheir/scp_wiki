09511408109e544ae4e60c73496ddbb3bcd2791d
MikuFanGirl <44828102+Bluheir@users.noreply.github.com>
2025-12-18 03:02:40 +1100

    fix: use paraglide keys to display toast errors

--- src/routes/profile/[userId]/+page.svelte
+++ src/routes/profile/[userId]/+page.svelte
@@ -8,6 +8,7 @@
 	import { superForm } from "sveltekit-superforms"
 	import { profileSchema } from "$lib/components/profile/profile"
 	import { toast } from "svelte-sonner"
+	import { m } from "$lib/paraglide/messages"
 
 	const { data }: PageProps = $props()
 	let { profile, supabase, readonly } = $derived(data)
@@ -27,10 +28,10 @@
 
 			switch(result.status) {
 				case 403:
-					toast.error("You no longer have permission to edit this profile", { class: "alert alert-error" })
+					toast.error(m.profile_noPermission(), { class: "alert alert-error" })
 					break
 				case 500:
-					toast.error("500: Internal server error", { class: "alert alert-error" })
+					toast.error(m.generic_error500(), { class: "alert alert-error" })
 					break
 			}
 		}
