626b231bb3121fad515415cc08d20de39a658ea6
MikuFanGirl <44828102+Bluheir@users.noreply.github.com>
2025-12-18 18:17:21 +1100

    test: create test fixture for a user

--- e2e/fixtures.ts
+++ e2e/fixtures.ts
@@ -1,11 +1,58 @@
 import { test as base } from "@playwright/test"
-import { createClient, SupabaseClient } from "@supabase/supabase-js"
+import { createClient, SupabaseClient, User } from "@supabase/supabase-js"
 
 export const test = base.extend<{ supabaseAdmin: SupabaseClient }>({
 	supabaseAdmin: async ({}, use) => {
 		const client = createClient(process.env.PUBLIC_SUPABASE_URL!, process.env.PRIVATE_SUPABASE_KEY!)
 
 		await use(client)
+	}
+})
+
+export const userTest = test.extend<{
+	user: {
+		userInfo: {
+			id: string
+			username: string
+			email: string
+			password: string
+		}
+		user: User
+		client: SupabaseClient
+	}
+}>({
+	user: async ({ page, supabaseAdmin }, use) => {
+		const username = "test"
+		const email = `test${Date.now()}@example.com`
+		const password = "12345aA!"
+		const { data } = await supabaseAdmin.auth.signUp({
+			email,
+			password,
+			options: { data: { username } }
+		})
+
+		if (!data || !data.user || !data.session) {
+			throw new Error("Failed to create user")
+		}
+
+		await page.goto("/login")
+		await page.getByLabel("Email").fill(email)
+		await page.getByLabel("Password").fill(password)
+		await page.locator("[type=submit]").click()
+
+		await use({
+			userInfo: { id: data.user.id, username, email, password },
+			client: createClient(
+				process.env.PUBLIC_SUPABASE_URL!,
+				process.env.PUBLIC_SUPABASE_ANON_KEY!,
+				{
+					accessToken: async () => data.session!.access_token
+				}
+			),
+			user: data.user
+		})
+
+		await supabaseAdmin.auth.admin.deleteUser(data.user.id, false)
 	}
 })
 
