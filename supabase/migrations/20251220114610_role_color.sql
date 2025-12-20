88e0e86869e1f0fa0379845f84a507aafca7d442
CJ-programming <coreycooldude99@gmail.com>
2025-12-20 23:00:46 +1100

    feat: Added supabase migration for adding colour to role

---
+++ supabase/migrations/20251220114610_role_color.sql
@@ -0,0 +1,11 @@
+BEGIN;
+CREATE DOMAIN color AS TEXT
+CHECK (
+	VALUE ~* '^#([0-9a-f]{3}|[0-9a-f]{6})$'
+	OR VALUE ~* '^rgb\(\s*(?:[01]?\d?\d|2[0-4]\d|25[0-5])\s*,\s*(?:[01]?\d?\d|2[0-4]\d|25[0-5])\s*,\s*(?:[01]?\d?\d|2[0-4]\d|25[0-5])\s*\)$'
+);
+
+ALTER TABLE urole_custom
+ADD role_color color
+DEFAULT 'rgb(43, 215, 251)';
+END;
