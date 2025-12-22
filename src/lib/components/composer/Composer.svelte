c2c02bb9f31767ca7d4caaf21e3494dd72fe78b0
MikuFanGirl <44828102+Bluheir@users.noreply.github.com>
2025-12-22 18:00:43 +1100

    feat: create `Composer` and `ComposerToolbar` components

---
+++ src/lib/components/composer/Composer.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { Editor } from "@tiptap/core"
+	import Superscript from "@tiptap/extension-superscript"
+	import History from "@tiptap/extension-history"
+	import Subscript from "@tiptap/extension-subscript"
+	import BulletList from "@tiptap/extension-bullet-list"
+	import OrderedListExt from "@tiptap/extension-ordered-list"
+	import ListItem from "@tiptap/extension-list-item"
+	import Link from "@tiptap/extension-link"
+	import FontFamily from "@tiptap/extension-font-family"
+	import { TextStyle } from "@tiptap/extension-text-style"
+	import Placeholder from "@tiptap/extension-placeholder"
+	import { Italic as ItalicExt } from "@tiptap/extension-italic"
+	import { Bold as BoldExt } from "@tiptap/extension-bold"
+	import Underline from "@tiptap/extension-underline"
+	import TextAlign from "@tiptap/extension-text-align"
+	import Document from "@tiptap/extension-document"
+	import Heading from "@tiptap/extension-heading"
+	import Paragraph from "@tiptap/extension-paragraph"
+	import Text from "@tiptap/extension-text"
+	import { onDestroy, onMount } from "svelte"
+	import { m } from "$lib/paraglide/messages"
+	import ComposerToolbar from "./ComposerToolbar.svelte"
+
+	let element: Element | undefined = $state()
+	let editorState: { editor: Editor | null } = $state({ editor: null })
+
+	onMount(() => {
+		editorState.editor = new Editor({
+			element,
+			extensions: [
+				ItalicExt,
+				BoldExt,
+				Link,
+				Underline,
+				Paragraph,
+				Heading,
+				Text,
+				TextAlign.configure({
+					alignments: ["left", "center", "right", "justify"],
+					types: ["heading", "paragraph"]
+				}),
+				Placeholder.configure({
+					placeholder: m.composer_placeholder()
+				}),
+				TextStyle,
+				FontFamily,
+				ListItem,
+				Document,
+				BulletList,
+				OrderedListExt,
+				Superscript,
+				Subscript,
+				History
+			],
+			autofocus: true,
+			onTransaction: ({ editor }) => {
+				editorState = { editor }
+			}
+		})
+	})
+	onDestroy(() => {
+		if (editorState.editor) {
+			editorState.editor.destroy()
+		}
+	})
+</script>
+
+{#if editorState.editor}
+	<ComposerToolbar editor={editorState.editor} />
+{/if}
+<div
+	class="border-base-content/10 border-y px-2 transition focus-within:border-primary hover:cursor-text"
+	bind:this={element}
+></div>
+
+<style>
+	:global(.tiptap):focus {
+		outline: none;
+	}
+	:global(.tiptap) {
+		box-sizing: border-box;
+	}
+	:global(.tiptap p.is-editor-empty:first-child::before) {
+		content: attr(data-placeholder);
+		float: left;
+		height: 0;
+		pointer-events: none;
+	}
+
+	:global(.tiptap *) {
+		all: revert;
+	}
+	:global(.tiptap a) {
+		cursor: pointer;
+	}
+</style>
---
+++ src/lib/components/composer/ComposerToolbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Editor } from "@tiptap/core"
+	import { m } from "$lib/paraglide/messages"
+	import * as Icon from "lucide-svelte"
+
+	const { editor }: { editor: Editor } = $props()
+</script>
+
+<div class="flex flex-wrap gap-1.5 rounded border border-base-content/10 items-center px-2 py-1">
+	<button
+		class="btn btn-ghost px-1 py-0.5"
+		title={m.composer_undo()}
+		onclick={() => editor.commands.undo()}
+	>
+		<Icon.RotateCcw class="w-[1em]" />
+	</button>
+	<button
+		class="btn btn-ghost px-1 py-0.5"
+		title={m.composer_redo()}
+		onclick={() => editor.commands.redo()}
+	>
+		<Icon.RotateCw class="w-[1em]" />
+	</button>
+</div>
