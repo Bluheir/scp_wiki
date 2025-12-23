2e1e4ca69c077b9cdfd8006bcbf29870937931b4
MikuFanGirl <44828102+Bluheir@users.noreply.github.com>
2025-12-23 18:53:34 +1100

    feat: allow composer to take in an editor

--- src/lib/components/composer/Composer.svelte
+++ src/lib/components/composer/Composer.svelte
@@ -1,72 +1,24 @@
 <script lang="ts">
-	import { Editor } from "@tiptap/core"
-	import { Markdown } from "@tiptap/markdown"
-	import Superscript from "@tiptap/extension-superscript"
-	import History from "@tiptap/extension-history"
-	import Subscript from "@tiptap/extension-subscript"
-	import BulletList from "@tiptap/extension-bullet-list"
-	import OrderedListExt from "@tiptap/extension-ordered-list"
-	import ListItem from "@tiptap/extension-list-item"
-	import Link from "@tiptap/extension-link"
-	import FontFamily from "@tiptap/extension-font-family"
-	import { TextStyle } from "@tiptap/extension-text-style"
-	import Placeholder from "@tiptap/extension-placeholder"
-	import { Italic as ItalicExt } from "@tiptap/extension-italic"
-	import { Bold as BoldExt } from "@tiptap/extension-bold"
-	import Underline from "@tiptap/extension-underline"
-	import TextAlign from "@tiptap/extension-text-align"
-	import Document from "@tiptap/extension-document"
-	import Heading from "@tiptap/extension-heading"
-	import Paragraph from "@tiptap/extension-paragraph"
-	import Text from "@tiptap/extension-text"
 	import { onDestroy, onMount, type Snippet } from "svelte"
-	import { m } from "$lib/paraglide/messages"
+	import { createSimpleEditor } from "./editor"
+	import type { Editor } from "@tiptap/core"
 
 	const {
+		editor = createSimpleEditor(),
 		children
 	}: {
+		editor?: Editor
 		children: Snippet<[{ editor: Editor; contents: Snippet<[]> }]>
 	} = $props()
 
 	let element: Element | undefined = $state()
-	let editorState: { editor: Editor } = $state({
-		editor: new Editor({
-			extensions: [
-				Markdown,
-				ItalicExt.configure(),
-				BoldExt,
-				Link,
-				Underline,
-				Paragraph,
-				Heading,
-				Text,
-				TextAlign.configure({
-					alignments: ["left", "center", "right", "justify"],
-					types: ["heading", "paragraph"]
-				}),
-				Placeholder.configure({
-					placeholder: m.composer_placeholder()
-				}),
-				TextStyle,
-				FontFamily,
-				ListItem,
-				Document,
-				BulletList,
-				OrderedListExt,
-				Superscript,
-				Subscript,
-				History
-			],
-			autofocus: true,
-			contentType: "markdown",
-			enableInputRules: false,
-			onTransaction: ({ editor }) => {
-				editorState = { editor }
-			}
-		})
-	})
+	let editorState: { editor: Editor } = $state({ editor })
 
 	onMount(() => {
+		editor.commands.clearContent()
+		editorState.editor.on("transaction", ({ editor }) => {
+			editorState = { editor }
+		})
 		editorState.editor.mount(element!)
 	})
 
